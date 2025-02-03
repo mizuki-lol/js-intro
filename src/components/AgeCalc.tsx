63a3e1d4fd12e45a421738bb7d5c55bfd0d5c5a0
xalfer <xaf.atmes@gmail.com>
2025-02-03 13:32:56 +0100

    refactor structure and add age calculator
    all components have been moved to the components directory
    and a widged to calculate birth year based on age has been added

---
+++ src/components/AgeCalc.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { calcAge } from '../utils';
+
+export default function AgeCalc() {
+  // Nějaké reaktivní proměnné? Možná věk? možná rok narození?
+  let [age, setAge] = useState(null)
+  let [birthYear, setBirthYear] = useState(null)
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setBirthYear(value);
+    if (value && !isNaN(value)) {
+      setAge(calcAge(parseInt(value, 10)));
+    } else {
+      setAge(null);
+    }
+  };
+
+  return (
+    <div>
+      <label>
+        Zadej rok narození:
+        <input type="number" value={birthYear} onChange={handleChange} />
+      </label>
+      {age !== null && <p>Tvůj věk je: {age}</p>}
+    </div>
+  );
+}
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export function calcAge(age) {
+  const now = new Date().getFullYear()
+  
+  return now - age
+}
